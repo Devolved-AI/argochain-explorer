6440c1f9bfd8aff87ee1da9341f1b5efddb1e207
Christian Kessler <psyferpunk@proton.me>
2024-07-10 14:57:18 -0700

    Update and rename init_db.js to initDb.js
    Signed-off-by: Christian Kessler <psyferpunk@proton.me>

--- initDb.js
+++ initDb.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./argochain_data.db');
+const db = new sqlite3.Database('./substrate_data.db');
 
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS blocks (number INTEGER PRIMARY KEY, hash TEXT)");
